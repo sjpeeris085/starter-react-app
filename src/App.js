6579de969816f808acf61a56122586141b952c1b
sjpeeris085 <tombraider085@gmail.com>
2022-12-19 21:48:25 +0530

    dd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container } from "@material-ui/core";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-function App() {
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
+import PostDetails from "./components/PostDetails/PostDetails";
+
+const App = () => {
+  const google_client_id =
+    "1031907545601-07e0cnlt2s3slr96mvspima5hagogq26.apps.googleusercontent.com";
+  //Best practice:- clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}
+
+  const user = JSON.parse(localStorage.getItem("profile"));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GoogleOAuthProvider clientId={google_client_id}>
+      <BrowserRouter>
+        <Container maxWidth="xl">
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={() => <Redirect to="/posts" />} />
+            <Route path="/posts" exact component={Home} />
+            <Route path="/posts/search" exact component={Home} />
+            <Route path="/posts/:id" component={PostDetails} />
+            <Route
+              path="/auth"
+              exact
+              component={() => (!user ? <Auth /> : <Redirect to="/posts" />)}
+            />
+          </Switch>
+        </Container>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   );
-}
+};
 
 export default App;
